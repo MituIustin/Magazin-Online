638f90a7f289c93d6920526abf254e206eb444d6
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-12-17 18:55:45 +0200

    TEST

---
+++ Magazin Online/Migrations/20231217165238_reparation2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Magazin_Online.Migrations
+{
+    public partial class reparation2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
